5b8b367d3db60f0e774d19c1f9bb33ce2062d00a
maxhyun <max.ghyun@gmail.com>
2019-04-19 14:40:33 +0800

    edit index js scss

--- js/all.js
+++ js/all.js
@@ -4,8 +4,7 @@ function selCity(){
     let str = [];
     str.County = city.value;
     const xhr = new XMLHttpRequest();
-    xhr.open('post','https://json2jsonp.com/?url=https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0002-001?Authorization=CWB-BF7B2448-C0EB-4CF4-B028-A16209EFDC55&format=JSON&locationName=',true);
-    xhr.setRequestHeader('Content-type','application/json');
+    xhr.get('get','https://obscure-crag-88418.herokuapp.com/travel',true);
     let data = JSON.stringify(str);
     xhr.send(data);
     xhr.onload = function(){
